65eb4e5491da2ef7c0cf341eb2bcfc0860c25b5f
Rick Huang <rick.liyue.huang@gmail.com>
2025-09-07 08:19:57 +1000

    add crud

---
+++ Backend/src/API/Models/Game.cs
@@ -0,0 +1,22 @@
+using System.ComponentModel.DataAnnotations;
+
+namespace API.Models;
+
+public class Game
+{
+    public Guid Id { get; set; }
+    
+    [Required]
+    [StringLength(50)] 
+    public required string Name { get; set; }
+    
+    [Required]
+    [StringLength(20)]
+    public required string Genre { get; set; }
+    
+    [Required]
+    [Range(1, 200)]
+    public decimal Price { get; set; }
+    
+    public DateOnly ReleaseDate { get; set; }
+}
--- Backend/src/API/Program.cs
+++ Backend/src/API/Program.cs
@@ -1,7 +1,106 @@
 
+using API.Models;
+
 var builder = WebApplication.CreateBuilder(args);
+
 var app = builder.Build();
 
-app.MapGet("/", () => "Hello World!");
+const string GetGameEndPointName = "GetGameName";
 
-app.Run();
+List<Game> games =
+[
+    new Game 
+    { 
+        Id = Guid.NewGuid(), 
+        Name = "The Legend of Zelda: Breath of the Wild", 
+        Genre = "Action-Adventure", 
+        Price = 59.99m, 
+        ReleaseDate = new DateOnly(2017, 3, 3) 
+    },
+    new Game 
+    { 
+        Id = Guid.NewGuid(), 
+        Name = "Red Dead Redemption 2", 
+        Genre = "Action-Adventure", 
+        Price = 49.99m, 
+        ReleaseDate = new DateOnly(2018, 10, 26) 
+    },
+    new Game 
+    { 
+        Id = Guid.NewGuid(), 
+        Name = "Elden Ring", 
+        Genre = "Action RPG", 
+        Price = 59.99m, 
+        ReleaseDate = new DateOnly(2022, 2, 25) 
+    },
+    new Game 
+    { 
+        Id = Guid.NewGuid(), 
+        Name = "Cyberpunk 2077", 
+        Genre = "Action RPG", 
+        Price = 49.99m, 
+        ReleaseDate = new DateOnly(2020, 12, 10) 
+    },
+    new Game 
+    { 
+        Id = Guid.NewGuid(), 
+        Name = "God of War Ragnarök", 
+        Genre = "Action-Adventure", 
+        Price = 69.99m, 
+        ReleaseDate = new DateOnly(2022, 11, 9) 
+    }
+];
+
+// GET /games
+app.MapGet("/games", () => games);
+
+// GET /games/{id}
+app.MapGet("/games/{id}", (Guid id) =>
+{
+    Game? game = games.Find(g => g.Id == id);
+    return game is null ? Results.NotFound() : Results.Ok(game);
+}).WithName(GetGameEndPointName);
+
+// POST /games
+app.MapPost("/games", (Game game) =>
+{
+    game.Id = Guid.NewGuid();
+    games.Add(game);
+    return Results.CreatedAtRoute(
+        GetGameEndPointName,
+        new { id = game.Id },
+        game);
+}).WithParameterValidation();
+
+// PUT /games/{id}
+app.MapPut("/games/{id}", (Guid id, Game updatedGame) =>
+{
+    var existingGame = games.Find(g => g.Id == id);
+    if (existingGame is null)
+    {
+        return Results.NotFound();
+    }
+
+    existingGame.Name = updatedGame.Name;
+    existingGame.Genre = updatedGame.Genre;
+    existingGame.Price = updatedGame.Price;
+    existingGame.ReleaseDate = updatedGame.ReleaseDate;
+
+    return Results.NoContent();
+}).WithParameterValidation();
+
+// DELETE /games/{id}
+app.MapDelete(("/games/{id}"), (Guid id) =>
+{
+    var existingGame = games.Find(g => g.Id == id);
+    if (existingGame is null)
+    {
+        return Results.NotFound();
+    }
+
+    games.Remove(existingGame);
+    return Results.NoContent();
+});
+
+app.Run(url: "http://localhost:5000"); // can change the url here
+
